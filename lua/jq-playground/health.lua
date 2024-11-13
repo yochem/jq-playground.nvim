83867a0ef6e351054bcd22b9c38b96f3de2c2909
Yochem van Rosmalen <git@yochem.nl>
2024-11-13 17:24:39 +0100

    feat(health): report custom keymaps

--- lua/jq-playground/health.lua
+++ lua/jq-playground/health.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 local function jq_found()
-  vim.health.start("Dependency: jq")
-  local ok, process = pcall(vim.system, { 'jq', '--version' }, {})
+  vim.health.start("jq CLI")
+  local ok, process = pcall(vim.system, { "jq", "--version" }, {})
 
   if not ok then
     vim.health.error("jq is not installed or not on your $PATH")
@@ -23,18 +23,36 @@ local function configuration()
 
   if vim.deep_equal(confmod.config, confmod.default_config) then
     vim.health.ok("using default configuration")
+  else
+    vim.health.info("Custom configuration:\n" .. vim.inspect(confmod.config))
   end
 
   if confmod.config.query_keymaps ~= nil then
     vim.health.warn("query_keymaps in config is deprecated. Use <Plug>(JqPlaygroundRunQuery)")
   end
+end
 
-  vim.health.info("Configuration:\n" .. vim.inspect(confmod.config))
+local function keymaps()
+  vim.health.start("Keymaps")
+  local found = false
+  -- TODO: https://github.com/neovim/neovim/pull/29464 wait till merged
+  local maps = vim.deepcopy(vim.api.nvim_get_keymap("n"))
+  vim.list_extend(maps, vim.api.nvim_get_keymap("i"))
+  for _, map in ipairs(maps) do
+    if map.rhs and string.match(map.rhs, "JqPlayground") then
+      vim.health.ok(("%s %s %s"):format(map.mode, map.lhs, map.rhs))
+      found = true
+    end
+  end
+  if not found then
+    vim.health.info("No keymaps found")
+  end
 end
 
 M.check = function()
   jq_found()
   configuration()
+  keymaps()
 end
 
 return M
