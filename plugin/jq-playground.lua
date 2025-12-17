84c2656709fa72ff13ea8dfd6d163c313e0e5fe0
Yochem van Rosmalen <git@yochem.nl>
2025-12-17 13:45:39 +0100

    fix: check if Nvim version is 0.10+
    Ref: #6

--- plugin/jq-playground.lua
+++ plugin/jq-playground.lua
@@ -3,4 +3,13 @@ if vim.g.loaded_jq_playground ~= nil then
 end
 vim.g.loaded_jq_playground = true
 
+if vim.version.range('>=0.10'):has(vim.version()) then
+  local msg = string.format(
+    'jq-playground requires Nvim v0.10+, you have Nvim v%s',
+    tostring(vim.version())
+  )
+  vim.notify_once(msg, vim.log.levels.WARN, {})
+  return
+end
+
 require("jq-playground").setup()
